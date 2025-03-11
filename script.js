2127845ab30acc81a0016cf860372cf1b6486c76
JoseParragaM <jgparraga99@gmail.com>
2025-03-10 22:19:17 -0500

    Jose - Class Activity 2 - 10 March 2025 #2

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ let age = 25;
 let isStudent = true;
 let carModel = undefined;
 const favoriteFood = null;
+
+console.log("Person first name is: " + firstName);
+console.log("Person age is: " + age);
+console.log("Is person a student?: " + isStudent);
+console.log("Person car model is: " + carModel);
+console.log("Person favorite food is: " + favoriteFood);
